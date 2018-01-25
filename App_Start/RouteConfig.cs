9cf188ad85029e884311e582c4a1056f7c562e33
mikettigit <acepost@protonmail.ch>
2018-01-25 16:06:29 +0300

    Pages controller

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,17 @@ namespace MvcApplication20
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            string[] pages = Directory.GetFiles(HttpContext.Current.Server.MapPath("/Views/Pages/"), "*.cshtml");
+            foreach (string page in pages)
+            {
+                string PageName = Path.GetFileNameWithoutExtension(page);
+                routes.MapRoute(
+                    PageName, // Route name
+                    PageName, // URL with parameters
+                    new { controller = "Pages", action = "Index", param1 = Path.GetFileName(page) } // Parameter defaults
+                );
+            }
 
             routes.MapRoute(
                 name: "Category",
